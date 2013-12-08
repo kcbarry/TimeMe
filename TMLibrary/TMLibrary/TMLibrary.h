067bb51c947fe6e9495efd50ac806751db0dbd77
Kenneth Barry <kennetcb@usc.edu>
2013-12-07 16:55:30 -0800

    Added library project, removed test framework

---
+++ TMLibrary/TMLibrary/TMLibrary.h
@@ -0,0 +1,13 @@
+//
+//  TMLibrary.h
+//  TMLibrary
+//
+//  Created by Clark Barry on 12/6/13.
+//  Copyright (c) 2013 KCBarry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMLibrary : NSObject
+
+@end
---
+++ TMLibrary/TMLibrary/TMLibrary.m
@@ -0,0 +1,13 @@
+//
+//  TMLibrary.m
+//  TMLibrary
+//
+//  Created by Clark Barry on 12/6/13.
+//  Copyright (c) 2013 KCBarry. All rights reserved.
+//
+
+#import "TMLibrary.h"
+
+@implementation TMLibrary
+
+@end
